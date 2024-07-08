87ece74dc603713b324e74fb332cc23118d2d9d1
qodql <88146038+qodql@users.noreply.github.com>
2024-07-08 18:23:48 +0900

    intro js test
    js 추가

--- js/sub.js
+++ js/sub.js
@@ -1,2 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    function animateElement(elementId, slideInClass, slideOutClass) {
+        const element = document.getElementById(elementId); // id로 해당 요소 찾기
+        const rect = element.getBoundingClientRect(); // 요소 위치 구하기
+        
+        // 요소가 화면 안쪽으로 들어왔을 경우 isVisible로 인식
+        const isVisible = rect.top <= window.innerHeight && rect.bottom >= 0; 
+      
+        if (isVisible) {
+          element.classList.add(slideInClass);
+          element.classList.remove(slideOutClass);
+        } else {
+          element.classList.add(slideOutClass);
+          element.classList.remove(slideInClass);
+        }
+      }
+      
+      window.addEventListener("scroll", function () {
+        animateElement("element-1", "fadein", "fadeout");
+      });
+
+
+
+
 })
