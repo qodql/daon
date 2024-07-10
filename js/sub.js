d95c60c5d5a094ca040cb95b55574a2735d72730
qodql <88146038+qodql@users.noreply.github.com>
2024-07-10 14:00:41 +0900

    branches commit1
    branches commit1

--- js/sub.js
+++ js/sub.js
@@ -1,41 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   
-    //intro grid image scroll event
-    function animateElement(elementClass, slideInClass) {
-        const element = document.getElementsByClassName(elementClass)[0]; // id로 해당 요소 찾기
-        const rect = element.getBoundingClientRect(); // 요소 위치 구하기
-        
-        // 요소가 화면 안쪽으로 들어왔을 경우 isVisible로 인식
-        const isVisible = rect.top <= window.innerHeight && rect.bottom >= 1200; 
-      
-        if (isVisible) {
-          element.classList.add(slideInClass);
-        }
-      }
-      
-      window.addEventListener("scroll", function () {
-        animateElement("intro-grid-wrapper", "intro-fadein");
-      });
-
-
-      //intro full bg scroll event
-      function animateElement2(elementClass, slideInClass) {
-        const element2 = document.getElementsByClassName(elementClass)[0]; 
-        const rect2 = element2.getBoundingClientRect(); 
-        
-        const isVisible2 = rect2.top <= window.innerHeight && rect2.top <= 350; 
-
-        if (isVisible2) {
-          element2.classList.add(slideInClass);
-        }
-      }
-      
-      window.addEventListener("scroll", function () {
-        animateElement2("intro-full-bg", "intro-expand");
-      });
-     
-     
+    
+    
 
 
     
