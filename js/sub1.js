934a0fb22535bd62e141ddb58d3fd07b2d6da2c3
kbyoon <qodqlqodql1206@gmail.com>
2024-07-22 01:01:46 +0900

    intro js

--- js/sub1.js
+++ js/sub1.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+
+
+
+
+    
+    function gridAnimate(elementClass, slideInClass){
+
+        const introElement = document.querySelector('.intro_grid_wrapper');
+        const introRect = introElement.getBoundingClientRect();
+
+
+        //const isVisible = introRect.top <= window.innerHeight && introRect.bottom >= 1200; 
+        const isVisible = introRect.top <= window.innerHeight; 
+        if(isVisible){
+            introElement.classList.add(slideInClass);
+        }
+    };
+
+    document.addEventListener("scroll", function(){
+        gridAnimate("intro_grid_wrapper", "intro_fadein");
+    },{once : true});
+
+
+
+
+
+
+
+
+
 })
