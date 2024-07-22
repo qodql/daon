500735b65f4b14e522579aae7d6206c048b78316
qodql <88146038+qodql@users.noreply.github.com>
2024-07-22 16:34:22 +0900

    sub1

--- js/sub1.js
+++ js/sub1.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
-
-
-
     
-    function gridAnimate(elementClass, slideInClass){
+    function gridAnimate(){
 
         const introElement = document.querySelector('.intro_grid_wrapper');
         const introRect = introElement.getBoundingClientRect();
 
-
         //const isVisible = introRect.top <= window.innerHeight && introRect.bottom >= 1200; 
-        const isVisible = introRect.top <= window.innerHeight; 
+        const isVisible = introRect.top <= 300; 
+
+        //console.log(introRect.top);
+
         if(isVisible){
-            introElement.classList.add(slideInClass);
+            introElement.classList.add('intro_fadein');
         }
+        isVisible && document.removeEventListener('scroll',gridAnimate);
     };
 
     document.addEventListener("scroll", function(){
-        gridAnimate("intro_grid_wrapper", "intro_fadein");
-    },{once : true});
+        gridAnimate();
+    });
+
+    
 
 
 
