f0d988fa3bbde9432ae4870cc4650bf88f31a222
JY Sung <96499160+0011git@users.noreply.github.com>
2024-08-01 12:04:27 +0900

    join checkbox, verify readonly

--- js/login.js
+++ js/login.js
@@ -1,2 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
+    /* 
+    // 비밀번호 패턴 검사 정규식
+    let pwChecker = (pw) => {
+        //정규식 조건: 영문소문자/숫자/특문 조합 8~15자
+        const pwPattern = /^(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*[a-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,15}$/;
+        //비번 추가조건1: 연속 3개 이상의 문자/숫자/특문이 오지 않도록
+        const pwAdditional1 = /(.)\1{2,}/;
+        //비번 추가조건2: 아이디와 4글자 이상 겹치지 않도록
+        // const pwAdditional2 = id.substring();
+
+        //비밀번호 패턴 검증
+        if(!pwPattern.test(pw)){
+            return false;
+        } else if(pwAdditional1.test(pw)){ //추가조건1
+            return false;    
+        // }
+        // else if(pwAdditional2.test(pw)){  //추가조건2
+        //     return false;
+        }else{
+            return true;
+        }
+    }
+    */
+
+    // 로그인+회원가입 공통
+    // 로그인
+    // 회원가입
+    // 회원가입 완료
 })
