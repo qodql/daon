0eb1d52ff96ee36b4f2be127afe61532a17c6075
Pon119 <hdy9316@gmail.com>
2024-08-05 17:05:47 +0900

    sub3_data

--- js/sub3.js
+++ js/sub3.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    var swiper = new Swiper(".rooftop-img", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".rooftop-bt",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+
 })
