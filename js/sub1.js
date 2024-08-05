b210fa8c86ebdd87e6017beb441b2a82a5db0153
qodql <88146038+qodql@users.noreply.github.com>
2024-08-05 18:21:52 +0900

    contact js

--- js/sub1.js
+++ js/sub1.js
@@ -1,94 +1,68 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("ss");
-
-    const elGrid = document.querySelectorAll('.intro_grid_wrapper p');
+    //Intro Grid Image Scroll Event
+    const introGrid = document.querySelectorAll('.intro_grid_wrapper p');
 
     const gridAnimate = new IntersectionObserver(function(entry){
-        entry.forEach((v)=>{
+        entry.forEach((v,i)=>{
             if(v.isIntersecting){
-                v.target.classList.add('intro_fadein');
+                setTimeout(()=>{
+                    v.target.classList.add('intro_fadein');
+                },50*i)
             }
         })
-    })
-    elGrid.forEach((v)=>{
+    });
+
+    introGrid.forEach((v)=>{
         gridAnimate.observe(v);
     })
 
-    testest
-    
-    
+    //Intro Full Background Scroll Event
+    const introFull = document.querySelectorAll('.intro_full_bg');
 
+    const expandAnimate = new IntersectionObserver(function(entry){
+        entry.forEach((v)=>{
+            if(v.isIntersecting){
+                v.target.classList.add('intro_expand');
+            }
+        })
+    })
 
-    
+    introFull.forEach((v)=>{
+        expandAnimate.observe(v);
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //intro grid image scroll event
-    /*
-    function gridAnimate(){
-
-        const introElement = document.querySelector('.intro_grid_wrapper');
-        const introRect = introElement.getBoundingClientRect();
-
-
-
-     
-        const isVisible = introRect.top <= 300; 
-    
-
-     
-
-        if(isVisible){
-            introElement.classList.add('intro_fadein');
-        }
-        isVisible && document.removeEventListener('scroll',gridAnimate);
+    //Contact KAKAO MAP
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.83117086809958, 127.46889840605908), // 지도의 중심좌표
+        level: 1 // 지도의 확대 레벨
     };
 
-    document.addEventListener("scroll", function(){
-        gridAnimate();
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+
+    // 마커가 표시될 위치입니다 
+    var markerPosition  = new kakao.maps.LatLng(37.83117086809958, 127.46889840605908); 
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: markerPosition
     });
 
-    //intro full bg scroll event
-    function expandAnimate(elementClass, slideInClass) {
-        
-        const element2 = document.getElementsByClassName(elementClass)[0]; 
-        const rect2 = element2.getBoundingClientRect(); 
-        
-        const isVisible2 = rect2.top <= window.innerHeight && rect2.top <= 350; 
-    
-            if(isVisible2) {
-                element2.classList.add(slideInClass);
-            }
-    }
-      
-    document.addEventListener("scroll", function() {
-        expandAnimate("intro_full_bg", "intro_expand");
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    var iwContent = '<div style="width=40px; padding:10px; text-align:center;">다온펜션</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwPosition = new kakao.maps.LatLng(20, 100); //인포윈도우 표시 위치입니다
+
+    // 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        position : iwPosition, 
+        content : iwContent 
     });
-
-
-
-    */
-
-
-
-
-
-
+  
+    // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+    infowindow.open(map, marker); 
 
 
 
