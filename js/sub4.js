88075b8cb9fd73d1ee403c27f2109ca9bf6c1679
JY Sung <96499160+0011git@users.noreply.github.com>
2024-08-05 15:07:24 +0900

    sub4_json

--- js/sub4.js
+++ js/sub4.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+    fetch("./sub4.json")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        const elName = document.querySelectorAll('.card_name');
+        const elName_kr = elName.querySelectorAll('strong');
+        const elName_eng = elName.querySelector('.card_name_eng p');
+        const elName_link = elName.querySelector('.card_name_eng a');
+
+        const elImg = document.querySelector('.card_img .imgbox img');
+        const elTxt = document.querySelector('.card_txt');
+
+        data.sub4_cards.forEach((card) => {
+            elName_kr.innerText += `${sub4_cards.name-kr}`;
+            elName_eng.innerText += `${sub4_cards.name-kr}`;
+        })
+    })
 
 })
