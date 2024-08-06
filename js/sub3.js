ba0d5e1ce798d4679dc0d603b595003250e20190
Pon119 <hdy9316@gmail.com>
2024-08-06 15:37:45 +0900

    sub3,sub5 css

--- js/sub3.js
+++ js/sub3.js
@@ -1,35 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
+  fetch("./data/sub3_facilities.json")
+    .then((res) => res.json())
+    .then((res) => {
+      let datacontents = function (n) {
+        let data,
+          info = "";
 
-fetch('./data/sub3_facilities.json')
-.then(res => res.json())
-.then(res =>{
-    console.log(res.items[2])
-    let data = res.items[0].rooftop[0];
-    let contents = document.querySelector('.contents')
+        switch (n) {
+          case 0:
+            data = res.items[0].bbq[0];
+            break;
+          case 1:
+            data = res.items[1].pool[0];
+            break;
+          case 2:
+            data = res.items[2].rooftop[0];
+            break;
+        }
 
-    let content =`<div class="sub3_rooftop_contents">
+        data.text.forEach((text) => {
+          info += `<div class="rooftop-s1">
+                        <dt>${text.subtext1}</dt>
+                        <dd>${text.subtext1inner}</dd>
+                    </div>`;
+        });
+
+        let contents = document.querySelector(".contents");
+
+        let content = `<div class="sub3_rooftop_contents">
                     <div class="rooftop-img">
                         <a><img src="${data.img[0]}" a href="#" /></a>
                         <a><img src="${data.img[1]}" a href="#" /></a>
                         <a><img src="${data.img[2]}" a href="#" /></a>
                     </div>
                     <div class="rooftop-title">
-                        <div class="rooftop-t">
-                        <p>${data.title[0]}</p>
-                        <p>${data.title[1]}</p>
-                        <p>${data.title[2]}</p>
-                    </div>
-                        <dl class="rooftop-s">
-                    <div class="rooftop-s1">
-                        <dt>${data.text[0].subtext1[0]}</dt>
-                        <dd>이용 가능 시간: 12:00  ~ 22:00</dd>
-                    </div>
-                    <div class="rooftop-s1">
-                        <dt>이용 안내</dt>
-                        <dd>
-                        우천 시 사용 불가 <br>
-                        허용되지 않은 화기류 사용 불가 </dd>
-                    </div>
+                    <div class="rooftop-t">
+                    <p>${data.title[0]}</p>
+                    <p>${data.title[1]}</p>
+                    <p>${data.title[2]}</p>
+                </div>
+                <dl class="rooftop-s">
+                    ${info}
+                    
                     </dl>
                     <div class="rooftop-bt">
                         <a href="#">바베큐 </a>
@@ -39,24 +51,30 @@ fetch('./data/sub3_facilities.json')
                     </div>
                     </div>`;
 
-    contents.innerHTML=content
+        contents.innerHTML = content;
 
-})
+        const button = document.querySelectorAll(".rooftop-bt a");
+        button.forEach(function (bt, i) {
+          bt.onclick = function (e) {
+            e.preventDefault();
+            datacontents(i);
+          };
+        });
+      };
+      datacontents(0);
+    });
 
-
-    var swiper = new Swiper(".rooftop-img", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        pagination: {
-          el: ".rooftop-bt",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
-
-
-})
+  //     var swiper = new Swiper(".rooftop-img", {
+  //         slidesPerView: 1,
+  //         spaceBetween: 30,
+  //         loop: true,
+  //         pagination: {
+  //           el: ".rooftop-bt",
+  //           clickable: true,
+  //         },
+  //         navigation: {
+  //           nextEl: ".swiper-button-next",
+  //           prevEl: ".swiper-button-prev",
+  //         },
+  //       });
+});
