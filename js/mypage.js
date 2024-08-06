7562bf9396233c57e9abbabecbfbe04836db5dd3
kbyoon <qodqlqodql1206@gmail.com>
2024-08-07 00:47:41 +0900

    mypage js

--- js/mypage.js
+++ js/mypage.js
@@ -1,23 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     
 
+    //Mypage Tab
+    const mypageTabBtn = document.querySelectorAll('.mypage_tap_wrap .tab li');
+    const mypageTabCont = document.querySelectorAll('.mypage_tap_wrap .tab_cont > div');
+
+    console.log(mypageTabBtn);
+    console.log(mypageTabCont);
 
 
-
-
-
-
-
-
-
-
-
-
-
+    mypageTabBtn.forEach((btn,key)=>{
+        btn.onclick = function(){
+            mypageTabBtn.forEach((v,i)=>{
+                mypageTabBtn[i].classList.remove('on');
+                mypageTabCont[i].classList.remove('on');
+            })
+            mypageTabBtn[key].classList.add('on');
+            mypageTabCont[key].classList.add('on');
+        }
+    })
 
 
 
     
-   
-
 })
