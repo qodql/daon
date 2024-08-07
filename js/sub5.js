a2b7c86bb4781a04dcb11c04d1fad2fc0d14f6fc
Infouse <hongyeong575@naver.com>
2024-08-07 17:03:25 +0900

    sub2 Json, sub5 script

--- js/sub5.js
+++ js/sub5.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 })
+
+
+
+
+
+
+
+
+
+
+
+//sub5_coumunity_review 스크립트
+
+const review_Btn = document.querySelectorAll('.images ul li a[name="pop_btn"]'),
+      review_Popup = document.querySelector('.popup');
+  review_btn.forEach((v)=>{
+    v.addEventListener('click', function(){
+      review_Popup.classList.add("active")
+    })
+  })
+      
