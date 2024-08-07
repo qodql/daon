bb85781c4436465e56c0d428334efc5ab65793e3
Lee-Hanjoo <gkswnehcl@gmail.com>
2024-08-07 11:47:06 +0900

    datepicker

--- js/common.js
+++ js/common.js
@@ -169,10 +169,61 @@ document.addEventListener("DOMContentLoaded", () => {
                 position : iwPosition, 
                 content : iwContent 
             });
-          
+
             // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
             infowindow.open(map, marker); 
         }
     })
+    
+    // [↓] datepicker
+    let today = new Date();
+    let tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    const day = document.querySelectorAll("ul li .date span")
+    const weekDay = ['일', '월', '화', '수', '목', '금', '토'];
+    const todayWeekDay = weekDay[today.getDay()];
+    const tomorrowWeekDay = weekDay[tomorrow.getDay()];
+    day.forEach((v,i)=>{
+        day[0].innerText = `(${todayWeekDay})`;
+        day[1].innerText = `(${tomorrowWeekDay})`;
+    })
+    let picker = tui.DatePicker.createRangePicker({
+        language: 'ko',
+        startpicker: {
+            date: today,
+            input: '#startpicker-input',
+            container: '#startpicker-container'
+        },
+        endpicker: {
+            date: tomorrow,
+            input: '#endpicker-input',
+            container: '#endpicker-container'
+        },
+        selectableRanges: [
+            [today, new Date(today.getFullYear() + 1, today.getMonth(), today.getDate())]
+        ],
+        format: 'YYYY. MM. dd',
+    });
 
+    function getDayOfWeek(date) {
+        if (!date) { return '날짜 선택'; }
+        var dayOfWeek = date.getDay();
+        var daysOfWeek = ['(일)', '(월)', '(화)', '(수)', '(목)', '(금)', '(토)'];
+        return daysOfWeek[dayOfWeek];
+    }
+    
+    picker.getStartpicker().on('change', function() {
+        var selectedDate = picker.getStartpicker().getDate();
+        day[0].innerHTML = `${getDayOfWeek(selectedDate)}`;
+    });
+    
+    picker.getEndpicker().on('change', function() {
+        var selectedDate = picker.getEndpicker().getDate();
+        day[1].innerHTML = `${getDayOfWeek(selectedDate)}`;
+        if(day[1].innerText !== '날짜 선택') { 
+            day[1].style.cssText = 'margin-left:0px;'
+        } else {
+            day[1].style.cssText = 'margin-left:-140px;'
+        }
+    });
 })
--- js/sub6.js
+++ js/sub6.js
@@ -1,44 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // calendar
-    var Calendar = tui.Calendar;
-
-    var today = new Date();
-    var firstMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-    var secondMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1);
-
-    var calendarLeft = new Calendar('#calendar.left', {
-        defaultView: 'month',
-        taskView: true,
-        scheduleView: true,
-        useCreationPopup: true,
-        useDetailPopup: true,
-        renderStartDate: firstMonth
-    });
-
-    var calendarRight = new Calendar('#calendar.right', {
-        defaultView: 'month',
-        taskView: true,
-        scheduleView: true,
-        useCreationPopup: true,
-        useDetailPopup: true,
-        renderStartDate: secondMonth
-    });
-
-    function moveCalendar(monthDiff) {
-        firstMonth.setMonth(firstMonth.getMonth() + monthDiff);
-        secondMonth.setMonth(secondMonth.getMonth() + monthDiff);
-
-        calendarLeft.setDate(firstMonth);
-        calendarRight.setDate(secondMonth);
-    }
-
-    document.getElementById('calendar_prev').addEventListener('click', function () {
-        moveCalendar(-1);
-    });
-
-    document.getElementById('calendar_next').addEventListener('click', function () {
-        moveCalendar(1);
-    });
 
     // [↓] step1 객실 수, 성인 수, 어린이 수 선택
     const roomInput = document.querySelector(".date_person .room input")
@@ -69,20 +29,31 @@ document.addEventListener("DOMContentLoaded", () => {
                     roomNum = 2;
                     roomInput.value = roomNum;
                 }
+                if(adultNum > (24 - childNum)){
+                    alert('한번에 예약 가능한 최대 인원 수는 24명 입니다.')
+                    adultNum = 24 - childNum;
+                    adultInput.value = adultNum;
+                }
             }
             if (targetLi.classList.contains("room")) {
-                if(roomNum > 3) {
-                    alert('한번에 예약 가능한 최대 객실 수는 4개 입니다.')
-                } else {
+                if(roomNum < 4) {
                     roomNum++;
-                }
+                    if (roomNum !== 4){
+                        v.style.cssText = 'opacity:1;'
+                    } else {
+                        v.style.cssText = 'opacity:0.3;'
+                    }
+                    if(roomNum >= 1) {
+                        minusBtn[i].style.cssText = 'opacity:0.3;'
+                    }
+                } 
                 roomInput.value = roomNum;
             }
             if (targetLi.classList.contains("child")) {
                 childNum++;
                 childInput.value = childNum;
                 if(childNum > (24 - adultNum)){
-                    alert(`예약 가능한 최대 어린이 수는 ${24 - adultNum}명 입니다.`)
+                    alert('한번에 예약 가능한 최대 인원 수는 24명 입니다.')
                     childNum = 24 - adultNum;
                     childInput.value = childNum;
                 }
@@ -104,6 +75,9 @@ document.addEventListener("DOMContentLoaded", () => {
             if (targetLi.classList.contains("room")) {
                 if(roomNum > 1) {
                     roomNum--;
+                    if (roomNum !== 4){
+                        plusBtn[i].style.cssText = 'opacity:1;'
+                    }
                 }
                 roomInput.value = roomNum;
             }
@@ -119,25 +93,22 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     // [↓] step1 객실 선택
-    const roomBtn = document.querySelectorAll(".room_select_btn")
+    const roomBtn = document.querySelectorAll(".room_select_btn");
+    let roomBtnSelected = '';
     roomBtn.forEach((v,i)=>{
-        roomBtn[i].addEventListener("click", (e) => {
-            const roomSelectedBtn = document.querySelectorAll(".room_select_btn.selected")
-            let roomSelectedBtnLength = roomSelectedBtn.length
-            if(roomNum - 1 < roomSelectedBtnLength) {
-                alert("선택할 수 있는 객실 수를 초과하였습니다.")
-            } else {
-                e.target.classList.add("selected")
+        v.addEventListener("click", (e)=>{
+            e.target.classList.toggle("selected");
+            roomBtnSelected = document.querySelectorAll(".room_select_btn.selected").length;
+            if(roomBtnSelected > roomNum) {
+                alert('선택 가능한 객실 수를 초과하였습니다.');
+                e.target.classList.remove("selected");
             }
-            console.log(roomSelectedBtn);
-            console.log(roomSelectedBtnLength);
-            // 1. 셀렉트 된 애를 다시 클릭했을때, 해제가 되어야함.
         })
     })
-
     nextBtn.addEventListener("click", ()=>{
-        // 2. 최소 roomNum개의 객실을 선택하세요. 선택했으면 페이지 이동
-        if(confirm(`객실 ${roomNum}개, 성인 ${adultNum}명, 어린이 ${childNum}명으로 예약을 진행하시겠습니까?`)) {
+        if (!roomBtnSelected) {
+            alert(`최소 ${roomNum}개의 객실을 선택해주세요.`);
+        } else {
             window.location.href='./sub6_reservation_step2.html'
         }
     })
