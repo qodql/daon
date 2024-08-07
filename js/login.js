afede9257ddc971bb685b286da8b6a7250949c2d
JY Sung <96499160+0011git@users.noreply.github.com>
2024-08-07 19:03:34 +0900

    naver login in progress

--- js/login.js
+++ js/login.js
@@ -25,17 +25,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // 로그인+회원가입 공통
     // 로그인
-    //서비스 주소
-    const SERVICE_URI = "http://127.0.0.1:5501/login.html";
-    //콜백 주소(작업 완료 후 메인으로 변경)
-    const REDIRECT_URI = "http://127.0.0.1:5501/login.html";
 
+    /* 공통 변수 */
+    //(1) 서비스 주소(로그인 버튼 있는 페이지)
+    const SERVICE_URI = "http://127.0.0.1:5501/login.html";
+    //(2) 콜백할 주소(리디렉트)
+    const REDIRECT_URI = "http://127.0.0.1:5501/login.html";
+    //(3) 콜백 후 이동할 주소(메인)
+    const LINK_URI = "http://127.0.0.1:5501/";
 
     /* 카카오 */
     const REST_API_KEY = "b87b0bf5c70402fe02aec9e63d71cf0a";
     let AUTHORIZE_CODE_KAKAO = new URLSearchParams(location.search),  //매번 랜덤하게 바뀜
         ACCESS_TOKEN_KAKAO = "";  //매번 랜덤하게 바뀜
-    const kakao = document.querySelector('.kakao');
+    const kakao = document.querySelector('.kakao'); //로그인 버튼
 
     // 1. 인가코드 받기(주소창에 파라미터(?code=형태)로 들어옴) //login.js
     function loginWithKakao() {
@@ -72,7 +75,7 @@ document.addEventListener("DOMContentLoaded", () => {
             })
         }
     }
-    displayToken();  // 2. 액세스 토큰 발급 실행
+    displayToken();
 
     //3. 사용자 정보 받기   //common.js(다른 페이지에서 로그인 정보 계속 사용)
     function userInfoFunc(){
@@ -107,36 +110,63 @@ document.addEventListener("DOMContentLoaded", () => {
 
     /* 네이버 */
     const CLIENT_ID = "cEjPiQLxtraGtftdtKot",
-          CLIENT_SECRET = "sXx4SV6rmO";
-    let ACCESS_TOKEN_NAVER = naver_id_login.getAccessToken();
-    const naver = document.querySelector('.naver');
-    //아래 변수 2개는 변경x
+          CLIENT_SECRET = "sXx4SV6rmO",
+          CALLBACK_URI = SERVICE_URI;
+          // 네이버가 콜백주소 url에 엑세스 토큰값을 보내기 때문에, 로그인 성공 시 바로 메인페이지로 이동하지 않습니다.
+          // 일단 다시 로그인 페이지로 다시 돌아온 후 받아온 토큰값과 회원정보를 저장하고, 이후에 메인으로 이동합니다.
     var naver_id_login = new naver_id_login(CLIENT_ID, CALLBACK_URI);
     var state = naver_id_login.getUniqState();
+    let ACCESS_TOKEN_NAVER = naver_id_login.getAccessToken();
 
-    // 1. 인가코드 받기: 네이버는 로그인 시 인가코드 필요 없는 것 같습니다....
+    const naver = document.querySelector('.naver'); //로그인버튼
+    let naverUserInfo = {
+        email:'',
+        nickname:'',
+        age:'',
+        id:'',
+        b_day:'',
+        gender:'',
+        mobile:''
+    };
+
+    // (추가) 쿠키 저장 함수
+    const setCookie = function() {
+        let endDate = new Date();
+        endDate.setSeconds(
+            endDate.getSeconds() + 86400  //쿠키 유지시간=24시간(단위:초)
+        );
+        Object.entries(naverUserInfo).forEach(([key, value]) => {
+            document.cookie = `${key}=${value}; expires=${endDate.toGMTString()};`;
+        })
+    }
+    
+    /* 네이버 로그인 초기화 Script */
+    // 1. 인가코드 받기: 네이버는 인가코드 필요 없나..?
     // 2. 액세스 토큰 발급 (주소창에 access_token=형태로 들어옴)
     const loginWithNaver = function(){
-        //리디렉트 페이지를 로그인 페이지로, 엑세스 토큰 쿠키에 저장 후 메인 페이지로 이동
-        location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=`;   //http%3A%2F%2F127.0.0.1%3A5500%2F24.naver_login.html
-        naverSignInCallback()
+        location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${CALLBACK_URI}&state=`   //http%3A%2F%2F127.0.0.1%3A5500%2F24.naver_login.html
     }
-    btn.addEventListener('click', loginWithNaver)
-    console.log(ACCESS_TOKEN_NAVER); //엑세스 토큰 잘 발급 받았는지 확인용
+    /* 네이버 로그인 Callback페이지 처리 Script */
+    naver_id_login.get_naver_userprofile("naverSignInCallback()");
+    // 네이버 사용자 프로필 조회 이후 프로필 정보를 처리할 callback function
+    function naverSignInCallback() {
+        //유저 정보 저장
+        naverUserInfo.email = naver_id_login.getProfileData('email');
+        naverUserInfo.nickname = naver_id_login.getProfileData('nickname');
+        naverUserInfo.age = naver_id_login.getProfileData('age');
+        naverUserInfo.id = naver_id_login.getProfileData('id');
+        naverUserInfo.b_day = naver_id_login.getProfileData('birthday');
+        naverUserInfo.gender = naver_id_login.getProfileData('gender');
+        naverUserInfo.mobile = naver_id_login.getProfileData('mobile');
+        console.log(naverUserInfo); //확인용
+        //쿠키 저장
+        setCookie();
+    }
 
-    // 3. 사용자 정보 받기   //common.js(다른 페이지에서 로그인 정보 계속 사용)
-    // 액세스 토큰 받은 이후 실행, 엑세스 토큰과 기타 회원 정보를 저장 후 메인 페이지로 이동시킴
-	function naverSignInCallback() {
-		while(ACCESS_TOKEN_NAVER !== null){
-            EMAIL_NAVER = naver_id_login.getProfileData('email');
-            NICKNAME_NAVER = naver_id_login.getProfileData('nickname');
-            console.log(EMAIL_NAVER);
-            console.log(NICKNAME_NAVER);
-        }
-        location.href = "http://127.0.0.1:5501/";
-	}
-	// 네이버 사용자 프로필 조회
-	naver_id_login.get_naver_userprofile(naverSignInCallback());
+    naver.addEventListener('click', function(e){
+        loginWithNaver();
+    })
+    console.log(ACCESS_TOKEN_NAVER);
 
 
     /* 구글 */
