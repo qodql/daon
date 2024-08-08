3c2cdae4547587b7231e173fb58255c413143a3a
Infouse <hongyeong575@naver.com>
2024-08-08 11:43:58 +0900

    sub5 js

--- js/sub5.js
+++ js/sub5.js
@@ -4,32 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
   //sub5_coumunity_review 스크립트
 
 
+// review_btn 이렇게 하거나 reviewBtn << 스크립트에선 이게 좋음
+  const popup_btn = document.querySelectorAll("[name='pop_btn']"),
+        popup = document.querySelector('.popup'),
+        popup_close = document.querySelector('.close_menu'),
+        body = document.body;
 
-  const review_Btn = document.querySelectorAll(".images ul li a[name='pop_btn']"),
-        review_Popup = document.querySelector('.popup'),
-        modal_close = document.querySelector('.closemenu'),
-        review_popbg = document.querySelector(".popup_bg"),
-        modal_scroll = document.body;
-
-  review_Btn.forEach((v)=>{  
+  popup_btn.forEach((v)=>{  
     v.addEventListener('click', function(){
-      review_Popup.classList.add("active");
-      review_Popup.classList.remove("unactive");
-      modal_scroll.style.overflow = "hidden";
+      popup.classList.add("active");
+      body.classList.add("scroll_hidden");//style로 넣지 말고 class 넣어서 제어
       
-      modal_close.onclick = function(e){
-        review_Popup.classList.remove("active");
-        review_Popup.classList.add("unactive");
-      modal_scroll.style.overflow = "unset"
+      popup_close.onclick = function(){
+        popup.classList.remove("active");
+        body.classList.remove("scroll_hidden")
       }
     })
     
   })
-  window.addEventListener('click', function(e){
-    if(e.target == review_Popup){
-      review_Popup.classList.remove("active");
-      review_Popup.classList.add("unactive");
-    modal_scroll.style.overflow = "unset"
+  popup.addEventListener('click', function(e){
+    if(e.target == popup){
+      popup.classList.remove("active");
+      body.classList.remove("scroll_hidden")
     }
   })
 
