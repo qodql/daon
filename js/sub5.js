03d761cc8ca4010b0f1113641fbd2cd82f15368f
qodql <88146038+qodql@users.noreply.github.com>
2024-08-08 11:47:15 +0900

    notice json

--- js/sub5.js
+++ js/sub5.js
@@ -1,4 +1,65 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //sub5_community_notice json
+  fetch("./data/sub5_community_notice.json")
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(data){
+    const pageReverse = data.community_notice.reverse();
+    //notice page
+    let page = [];
+    for(let i=0; i < pageReverse.length; i+=5){
+      page.push(pageReverse.slice(i,i+5));
+    }
+
+    let noticePaging = ()=>{
+      const pageUl = document.querySelector('.pagination');
+      const createLi = document.createElement('li');
+
+      page.forEach((v,i)=>{
+        createLi.innerHTML += `<button>${i+1}</button>`
+      })
+      pageUl.append(createLi);
+
+      const pageBtn = document.querySelectorAll('button');
+      pageBtn.forEach((btn,pageNum)=>{
+        btn.onclick = ()=>{
+          notice_data(pageNum);
+        }
+      })
+    }
+
+    //notice data insert
+    const noticeTable = document.querySelector('.notice-1 tbody');
+    
+    let notice_data = (n) =>{
+      noticeTable.innerHTML = '';
+
+      page[n].forEach((v,i)=>{
+        noticeTable.innerHTML += `<td>${v.board_num}</td>
+                                  <td>
+                                    <p>${v.board_title}</p>
+                                  </td>
+                                  <td>${v.board_date}</td>`;
+      })
+    }
+    notice_data(0);
+    noticePaging();
+
+
+
+
+
+
+  })
+
+
+
+
+
+
+
+
 
 })
 
