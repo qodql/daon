68f59d4dd1be159216e22e3cac51c2273d84faee
JY Sung <96499160+0011git@users.noreply.github.com>
2024-08-12 10:56:50 +0900

    googleLogin

--- js/login.js
+++ js/login.js
@@ -44,7 +44,7 @@ if(access_token.length && access_token[0].split('=')[1]){ //엑세스 토큰 값
     // location.href="/";
 } else{                         //엑세스 토큰 값없: 로그인 진행
 
-/* sns 로그인 공통 변수, 함수*/
+/* sns 로그인 공통 변수, 함수 ---------------------------------*/
 //(1) 서비스 주소(로그인 버튼 있는 페이지)
 const SERVICE_URI = "http://127.0.0.1:5501/login.html";
 //(2) 콜백할 주소(리디렉트)
@@ -60,7 +60,6 @@ var profile =  {
     b_day: '',
     gender: '',
     mobile: ''
-    // id: ''
 };
 //(5) 쿠키와 세션에 유저 정보를 저장하는 함수
 const setUserInfo = function(accessToken) {
@@ -80,15 +79,16 @@ const setUserInfo = function(accessToken) {
     login = true;
     sessionStorage.login = JSON.stringify(login);
         /* <참고> 세션에 저장한 정보 꺼내쓸 때 : 
-              let checkLogin = JSON.parse(sessionStorage.login); 
-              console.log(checkLogin);  //true면 로그인, false면 로그아웃 상태
-              이런 방법으로 사용  */
+            let checkLogin = JSON.parse(sessionStorage.login); 
+            console.log(checkLogin);  //true면 로그인, false면 로그아웃 상태
+            이런 방법으로 사용  
+        */
         /* 로그아웃 시 sessionStorage.clear() 반드시 해주기!!! */
     //4. 메인 페이지로 이동
     location.href = AFTER_REDIRECT_URI;
 }
 
-/* 카카오 */
+/* 카카오 ---------------------------------*/
 const kakaoLogin = function(){
     const REST_API_KEY = "b87b0bf5c70402fe02aec9e63d71cf0a";
     let AUTHORIZE_CODE_KAKAO = new URLSearchParams(location.search),  //인가코드는 매번 랜덤하게 바뀜
@@ -162,13 +162,13 @@ const kakaoLogin = function(){
 }
 kakaoLogin()
 
-/* 네이버 : 네이버는 함수안에 넣으면 스코프 이슈 발생, 못넣음*/
-const CLIENT_ID = "cEjPiQLxtraGtftdtKot",
-        CLIENT_SECRET = "sXx4SV6rmO",
-        CALLBACK_URI = SERVICE_URI;
+/* 네이버 : 네이버는 함수안에 넣으면 스코프 이슈 발생, 못넣음 ---------------------------------*/
+const NAVER_CLIENT_ID = "cEjPiQLxtraGtftdtKot",
+    //   CLIENT_SECRET = "sXx4SV6rmO",
+      CALLBACK_URI = SERVICE_URI;
         // 네이버가 콜백주소 url에 엑세스 토큰값을 보내기 때문에, 로그인 성공 시 바로 메인페이지로 이동하지 않습니다.
         // 일단 다시 로그인 페이지로 다시 돌아온 후 받아온 토큰값과 회원정보를 저장하고, 이후에 메인으로 이동합니다.
-var naver_id_login = new naver_id_login(CLIENT_ID, CALLBACK_URI);
+var naver_id_login = new naver_id_login(NAVER_CLIENT_ID, CALLBACK_URI);
 var state = naver_id_login.getUniqState();
 let ACCESS_TOKEN_NAVER = naver_id_login.getAccessToken();
 
@@ -178,7 +178,7 @@ const naver = document.querySelector('.naver'); //로그인버튼
 // 1. 인가코드 받기: 네이버는 인가코드 필요 없나..?
 // 2. 액세스 토큰 발급 (주소창에 access_token=형태로 들어옴)
 const loginWithNaver = function() {
-    location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${CALLBACK_URI}&state=`;
+    location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=token&client_id=${NAVER_CLIENT_ID}&redirect_uri=${CALLBACK_URI}&state=`;
 }
 /* 네이버 로그인 Callback페이지 처리 Script */
 naver_id_login.get_naver_userprofile("naverSignInCallback()");
@@ -197,5 +197,67 @@ naver.addEventListener('click', function(){
 })
 
 /* 구글 */
+const googleLogin = function(){
+    const GOOGLE_CLIENT_ID = '507623855565-u1kp5fvsfg2e263jpq2vmtage1rmmkcf.apps.googleusercontent.com';
+    const google = document.querySelector('.google');
+    
+    function signIn(){
+        //엔드포인트주소, 엑세스 토큰을 주고 유저정보를 받아오는 곳
+        var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+        //엑세스 토큰값을 서버에 주기 위해서 form생성
+        var form = document.createElement('form');
+        form.setAttribute('method', 'GET'); // Send as a GET request.
+        form.setAttribute('action', oauth2Endpoint);
+    
+        var params = {'client_id': GOOGLE_CLIENT_ID,
+                    'redirect_uri': REDIRECT_URI,
+                    'response_type': 'token',
+                    'scope': 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+                    'include_granted_scopes': 'true',
+                    'state': 'pass-through-value'};
+    
+        //form제출(submit)
+        for (var p in params) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'hidden');
+            input.setAttribute('name', p);  //client_id, redirect_uri. response_type, scope, include_granted_scopes, state
+            input.setAttribute('value', params[p]); //위의 name에 해당하는 각각의 값들
+            form.appendChild(input);
+        }
+        //dom에 엘리멘트 추가
+        document.body.appendChild(form);
+        //form자동제출(submit)
+        form.submit();
+    }
+    google.addEventListener('click', signIn);
+    
+    
+    //주소창에 들어온 access_token 뽑아서 저장하기
+    var fragmentString = location.hash.substring(1);
+    var params = {};
+    var regex = /([^&=]+)=([^&]*)/g, m;
+    while (m = regex.exec(fragmentString)) {
+        params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+    }
+    if (Object.keys(params).length > 0 && params['state']) {  
+        localStorage.setItem('authInfo', JSON.stringify(params)); //로컬스토리지에 엑세스토큰 저장하기
+        var authInfo = JSON.parse(localStorage.getItem('authInfo'));    //로컬스토리지에 저장한 엑세스토큰값 가져오기
+        // console.log(info);
+        console.log(authInfo['access_token']);  //엑세스토큰값
+        
+        fetch("https://www.googleapis.com/oauth2/v3/userinfo",{
+            headers:{
+                "Authorization": `Bearer ${authInfo['access_token']}`
+            }
+        })
+        .then((data) => data.json())    //데이터 받아서 json형태로 만들기
+        .then((info) => {
+            // console.log(info);
+            console.log(info.email);    //이메일주소
+            console.log(info.name);     //이름
+        })
+    }
+}
+googleLogin();
 
 }
