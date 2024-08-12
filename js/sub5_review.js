506614547f6788ef8f36a3848744d0adf724c4e9
Infouse <hongyeong575@naver.com>
2024-08-12 15:41:03 +0900

    sub2 js,json,css

--- js/sub5_review.js
+++ js/sub5_review.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded',()=> {
   fetch("../data/sub5_community_review.json")
-  .then((res)=>{res.json()})
-  .then((data)=> {
+  .then((res)=>{return res.json()})
+  .then((data)=>{ 
     const data_review = data.community_review;
-  })
+          review_thm = document.querySelector('.images > ul')
+          popup_wrap = document.querySelector('.review_wrap');
 
-  data_review.forEach(()=>{
-       
+
+    data_review.forEach((v)=>{
+      console.log(v.review_img)
+      review_thm.innerHTML += `
+      <li><a name="pop_btn"><img src="${v.review_img}"> </a></li>`
+      popup_wrap.innerHTML += ``
+    })
   })
 })
